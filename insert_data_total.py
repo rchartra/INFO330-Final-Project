a086c3a9337f4713cc5f625d4159036b3a4865c7
HibbaAbas <hibabbas@gmail.com>
2023-06-04 16:25:18 -0700

    clean up insert data for totals

--- insert_data_total.py
+++ insert_data_total.py
@@ -30,33 +30,12 @@ with open('admissions_releases_states.csv', 'r') as admissions_file:
         for admissions_row, populations_row in zip(admissions_csv_data, populations_csv_data):
             date = admissions_row[0]
             admissions = int(admissions_row[1])
-            admissions_white = int(admissions_row[2])
-            admissions_black = int(admissions_row[3])
-            admissions_hispanic = int(admissions_row[4])
-            admissions_amerind = int(admissions_row[5])
-            admissions_asian = int(admissions_row[6])
-            admissions_other = int(admissions_row[7])
             releases = int(admissions_row[8])
-            releases_white = int(admissions_row[9])
-            releases_black = int(admissions_row[10])
-            releases_hispanic = int(admissions_row[11])
-            releases_amerind = int(admissions_row[12])
-            releases_asian = int(admissions_row[13])
-            releases_other = int(admissions_row[14])
-            state = admissions_row[15]
 
             incarcerated_total = int(populations_row[2])
             incarcerated_white = int(populations_row[3])
-            incarcerated_black = int(populations_row[4])
-            incarcerated_hispanic = int(populations_row[5])
-            incarcerated_amerind = int(populations_row[6])
-            incarcerated_asian = int(populations_row[7])
-            incarcerated_other = int(populations_row[8])
 
-            incarceration_nonwhite = (
-                incarcerated_black + incarcerated_hispanic +
-                incarcerated_amerind + incarcerated_asian + incarcerated_other
-            )
+            incarceration_nonwhite = incarcerated_total - incarcerated_white
 
             #Get item_id from Date_State table
             cursor.execute("SELECT ID FROM Date_State WHERE Date=? AND State=?", (date, state))
